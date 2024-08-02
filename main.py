bf4a124e8f9d4ae7e1010646a63e258bf884fca5
Roman Timonin <rtimonin569@gmail.com>
2024-08-02 19:39:19 +0300

    update main

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-import concurrent
+import concurrent.futures
+import json
 import logging
+import re
+import time
 
+import click
 import requests
+from tabulate import tabulate
+from tqdm import tqdm
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def normalize_version(version: str) -> str:
+    """Нормализует версию, извлекая основную часть."""
+    match = re.match(r'(\d+(\.\d+){0,2})', version)
+    return match.group(0) if match else version
 
 
 def get_packages(url: str) -> list:
@@ -60,7 +72,18 @@ def compare_packages(branch1_packages: list, branch2_packages: list) -> dict:
     }
 
 
-def main(branch1: str, branch2: str) -> None:
+def save_results_to_json(result: dict, filename: str = 'comparison_result.json') -> None:
+    """Сохраняет результаты сравнения в JSON файл."""
+    with open(filename, 'w') as json_file:
+        json.dump(result, json_file, indent=4)
+
+
+@click.command()
+@click.argument('branch1')
+@click.argument('branch2')
+@click.option('--output', default='comparison_result.json',
+              help='Имя выходного файла для сохранения результата в формате JSON.')
+def main(branch1: str, branch2: str, output: str) -> None:
     """Основная функция для сравнения пакетов двух веток."""
     API_URL = "https://rdb.altlinux.org/api/export/branch_binary_packages/"
     with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -69,5 +92,31 @@ def main(branch1: str, branch2: str) -> None:
             executor.submit(get_packages, API_URL + branch2)
         ]
 
+        # Используем tqdm для отображения прогресса
+        for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures), desc="Загрузка пакетов"):
+            future.result()  # Просто ждем завершения
+
         branch1_packages = futures[0].result()
         branch2_packages = futures[1].result()
+
+        result = compare_packages(branch1_packages, branch2_packages)
+
+        save_results_to_json(result, output)
+        click.echo(f'Результаты сохранены в файл: {output}')
+
+        # Вывод результатов
+        click.echo("Пакеты только в ветке 1:")
+        time.sleep(2)
+        click.echo(tabulate(result['only_in_branch1'].values(), headers='keys'))
+
+        click.echo("\nПакеты только в ветке 2:")
+        time.sleep(2)
+        click.echo(tabulate(result['only_in_branch2'].values(), headers='keys'))
+
+        click.echo("\nПакеты с более высокой версией в ветке 1:")
+        time.sleep(2)
+        click.echo(tabulate(result['higher_in_branch1'].values(), headers='keys'))
+
+
+if __name__ == "__main__":
+    main()
